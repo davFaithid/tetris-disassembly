9ff1c7648961d5f6a4009d1a13ad22b57b5e58a3
davFaithid <42425325+davFaithid@users.noreply.github.com>
2019-09-22 17:16:29 -0400

    Update game.asm

--- game.asm
+++ game.asm
@@ -1,6 +1,4 @@
 ; Disassembly of "Tetris.gb"
-; This file was created with mgbdis v1.3 - Game Boy ROM disassembler by Matt Currie.
-; https://github.com/mattcurrie/mgbdis
 
 INCLUDE "hardware.inc"
 INCLUDE "bank_000.asm"
